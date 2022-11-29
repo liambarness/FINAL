f59372ed2d951248639a35b0e7f53304917aef58
marvd0g <77862969+marvd0g@users.noreply.github.com>
2022-11-28 20:32:43 -0500

    Working Scraping of full messages into json

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
-wait what
+import requests
+import json
+import discord
+
+
+#Scraping Messages
+def get_msgs(channelID):
+    #Discord Auth code for test account
+    headers = { 'authorization': 'MTA0Njk1ODgzMjY0MDg1MjAyOQ.Gk01lg.8dOUWq2l4zSPqvJa31rEL17cC67mCKHntmWNw4' }
+    r = requests.get(f'https://discord.com/api/v9/channels/{channelID}/messages', headers=headers)
+    jsonobj = json.loads(r.text)
+    for key in jsonobj:
+        print(key, '\n')
+
+get_msgs(1046959546574311527)
+#Gets full message information!
