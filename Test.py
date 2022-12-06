c2d8763c959510d4441319056325defd625f8cd6
marvd0g <77862969+marvd0g@users.noreply.github.com>
2022-12-05 23:09:36 -0500

    WORKING!!!!

--- Test.py
+++ Test.py
@@ -1,31 +1,42 @@
 import requests
 import json
 import discord
+from discord.ext import commands,tasks
 
 #Discord bot setup!!
-TOKEN = 'MTA0OTUxMzc2Nzc0ODI0NzU1Mg.GDvAUH.4hfqn3GCTJBSj_KWUxM6vILkXVjxzsiTHhfo3w'
+TOKEN = 'MTA0OTUxMzc2Nzc0ODI0NzU1Mg.GL6QEH.7oy9Ukmo9f_wlE07CfGuYzpl7J3OsIs6x6UIuU'
 client = discord.Client()
 
 
 #Message fetcher
 def fetchMessages(channelid):
-    messages = 0
     headers = {
-        'authorization': 'MjAzNTMxMDI2NzAwNDM1NDU2.GPiVs6.sWsrUKz4o5PqO5_YcJOxr1TSbQJe_rJM8b9lP4'
+        'authorization': 'MjAzNTMxMDI2NzAwNDM1NDU2.GVABcf.HpYB-uz0PTtpmJ6apJAXXO_O2eDBudnd04aIDI'
     }
     r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages?limit=50', headers=headers)
     jsonobj = json.loads(r.text)
-    for vals in jsonobj:
-        messages += 1
-        content = (vals['content'])
+    content = []
+    for val in jsonobj:
+        if val['content'].strip():
+            content.append(val['content'])
     return content
-fetchMessages('1049502790386462732')
 
-def test():
-    for num in range(6):
-        return 'test'
+
+print(fetchMessages('1049502790386462732'))
+
+
+
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-    await client.get_channel(1049502824570040370).send(fetchMessages('1049502790386462732'))
+    for msgs in fetchMessages('1049502790386462732'):
+        await client.get_channel(1049502824570040370).send(msgs)
+    # channel = client.get_channel(1049502824570040370)
+    # index = 0
+    # # while index < len(fetchMessages('1049502790386462732')):
+    # #     message = (fetchMessages('1049502790386462732'))[index]
+    # #     channel.send(message)
+    # #     index +- 1
+
+
 client.run(TOKEN)
